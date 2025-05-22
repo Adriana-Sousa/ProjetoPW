dbfcc4ef8bc74ab137b82d1262b1386815fc3833
Adriana Sousa <120699927+Adriana-Sousa@users.noreply.github.com>
2025-05-21 23:48:54 -0300

    navbar

--- Projeto/frontend/src/components/navbar/navbar.jsx
+++ Projeto/frontend/src/components/navbar/navbar.jsx
@@ -1,5 +1,5 @@
 import styles from './navbar.module.css'
-//import { LuShoppingCart, LuUserCircle, LuMenu } from "react-icons/lu"
+import { LuShoppingCart, LuUser, LuMenu } from "react-icons/lu"
 import { Drawer } from '@mui/material'
 import { useState } from 'react'
 
@@ -11,7 +11,36 @@ import { useState } from 'react'
     }
 
     return (
-        <h1>Navbar</h1>
+        <nav className={styles.navbarContainer}>
+            <div className={styles.navbarItems}>
+                 <img className={styles.logo} src="/logo.png" alt="" />
+                 <div className={styles.navbarLinksContainer}>
+                    <a href="" className={styles.navbarLink}>Home</a>
+                    <a href="" className={styles.navbarLink}>Plates</a>
+                     <LuShoppingCart className={styles.navbarLink} />
+                    <LuUser className={styles.navbarLink} />
+                 </div>
+            </div>
+
+              <div className={styles.mobileNavbarItems}>
+                <img className={styles.logo} src="/logo.png" alt="" />
+                <div className={styles.mobileNavbarBtns}>
+                    <LuShoppingCart className={styles.navbarLink} />
+                    <LuMenu className={styles.navbarLink} onClick={handleOpenMenu}/>
+                </div>
+            </div>
+            <Drawer
+            anchor='right'
+            open={openMenu}
+            onClose={handleOpenMenu}
+            >
+                <div className={styles.drawer}>
+                    <a href="" className={styles.navbarLink}>Home</a>
+                    <a href="" className={styles.navbarLink}>Plates</a>
+                    <a href="" className={styles.navbarLink}>Profile</a>
+                </div>
+            </Drawer>
+        </nav>
     )
 }
 

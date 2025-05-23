1e91d635697796cf6dd7e0da70af8c77f8922fb8
Natália Lima <107011521+NattLima@users.noreply.github.com>
2025-05-22 22:59:51 -0300

    Update App.jsx

--- Projeto/frontend/src/App.jsx
+++ Projeto/frontend/src/App.jsx
@@ -1,19 +1,26 @@
-import Navbar from './components/navbar/navbar'
-import Footer from './components/footer/footer'
-import { useState } from 'react'
-import { Outlet } from "react-router-dom"
-
+import Navbar from "./components/navbar/navbar";
+import Footer from "./components/footer/footer";
+import { useState } from "react";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  	return (
-		<>
-			<Navbar />
-			<Outlet />
-		
-		</>
-	)
+  return (
+    <>
+      <Navbar />
+
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+
+      <Outlet />
+
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;

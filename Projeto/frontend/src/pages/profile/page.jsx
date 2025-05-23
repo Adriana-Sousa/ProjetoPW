33974f0372faa36d041c83b8337b6911dddb7ef3
Natália Lima <107011521+NattLima@users.noreply.github.com>
2025-05-22 22:51:34 -0300

    Update page.jsx

--- Projeto/frontend/src/pages/profile/page.jsx
+++ Projeto/frontend/src/pages/profile/page.jsx
@@ -1,5 +1,16 @@
-export default function Profile() {
-    return (
-        <h1>perfil</h1>
-    )
+import React from "react";
+import styles from "./page.module.css";
+
+function Profile() {
+  return (
+    <div className={styles.container}>
+      <h1>Perfil do Usuário</h1>
+      <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.".</p>
+    </div>
+  );
 }
+
+export default Profile;

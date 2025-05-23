907e0884084fefe2b45ecc8c6aa68ef49c981526
Natália Lima <107011521+NattLima@users.noreply.github.com>
2025-05-22 22:53:17 -0300

    Update page.jsx

--- Projeto/frontend/src/pages/home/page.jsx
+++ Projeto/frontend/src/pages/home/page.jsx
@@ -1,11 +1,19 @@
-import styles from './page.module.css'
 //import Dessert from '../../../public/imgs/homepage/dessert'
 //import NaturalFood from '../../../public/imgs/homepage/naturalFood'
 //import Vegetable from '../../../public/imgs/homepage/vegetable'
 //import { FaMapMarkerAlt, FaFacebookSquare, FaInstagram, FaWhatsapp } from "react-icons/fa"
+import React from "react";
+export default Home;
+import Login from "../pages/login/page.jsx";
+import Cadastro from "../pages/cadastro/page.jsx";
 
-export default function Home() {
-    return (
-        <h1>home</h1>
-    )
+function Home() {
+  return (
+    <div>
+      <h1>Bem-vindo à Home</h1>
+      <Login />
+      <Cadastro />
+      <p>Esta é a página inicial do sistema.</p>
+    </div>
+  );
 }

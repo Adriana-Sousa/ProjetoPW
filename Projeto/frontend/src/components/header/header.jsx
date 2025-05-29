ebef22bd8e0ac5d9f1ad3360354c3ba55064fa23
Natália Lima <107011521+NattLima@users.noreply.github.com>
2025-05-28 23:24:03 -0300

    add header

---
+++ Projeto/frontend/src/components/header/header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <header>
+      {/* Aqui vai sua logo */}
+      <h1>BitStore</h1>
+      <nav>
+        <a href="/">Home</a> | 
+        <a href="/carrinho">Carrinho</a> | 
+        <a href="/login">Login</a>
+      </nav>
+    </header>
+  )
+}
+
+export default Header;

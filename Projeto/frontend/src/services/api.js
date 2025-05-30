6d9844a8cb9e272e5a7e95c60b31a9b72f836eb9
Natália Lima <107011521+NattLima@users.noreply.github.com>
2025-05-30 01:01:01 -0300

    Create api.js

---
+++ Projeto/frontend/src/services/api.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
+  withCredentials: true, // Permite o envio de cookies e credenciais            
+  // headers: {
+  //   'Authorization': `Bearer ${localStorage.getItem('auth')?.token}`
+  // }  
+})
+
+export default api;

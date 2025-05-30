dedfea7e29d3a34938019d3eb16f39b70782b80a
Adriana Sousa <120699927+Adriana-Sousa@users.noreply.github.com>
2025-05-30 00:38:12 -0300

    autenticação do admin

--- Projeto/frontend/src/pages/loginadm/loginadm.jsx
+++ Projeto/frontend/src/pages/loginadm/loginadm.jsx
@@ -1,9 +1,49 @@
 import './loginadm.css';
-import bgImage from '../../assets/FOTOBASE.jpg';
+import bgImage from '../../assets/FOTOBASE.png';
 import { Link } from 'react-router-dom';
 import { FiHome } from 'react-icons/fi';
+import { TextField } from "@mui/material"
+import { useEffect } from 'react';
+import authServices from '../../services/auth';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-function Login() {
+function LoginAdm() {
+
+    const authData = JSON.parse(localStorage.getItem('auth'))
+    const { login, signup, authLoading } = authServices()
+    const [formData, setFormData] = useState(null)
+    const navigate = useNavigate()
+    
+        useEffect(() => {
+            if(authData) {
+                if (authData.user.role === "admin") {
+                    navigate('/admin')
+                }
+                else {
+                    navigate('/sem-autorizacao')
+                }
+                 
+            }
+        }, [authData])
+
+    // função para enviar forms
+    const handleSubmitForm = (e) => {
+        e.preventDefault()
+            console.log(formData)
+        login(formData)
+                
+    }
+
+     // função para mudar os dados
+    const handleFormDataChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
+        console.log(formData)
+    }
+
   return (
     <div 
       className="login-page" 
@@ -16,21 +56,24 @@ function Login() {
           </Link>
         </div>
       <h1>OLÁ ADMINISTRADOR</h1>
-      <div className="input-group">
+      <form onSubmit={handleSubmitForm}>
+        
+        <div className="input-group" >
         <span className="icon">🔒</span>
-        <input type="text" placeholder="User" />
+        <input type="email" name='email' placeholder="User" onChange={handleFormDataChange} required/>
       </div>
       <div className="input-group">
         <span className="icon">🔑</span>
-        <input type="password" placeholder="Senha" />
+        <input type="password" name='password' placeholder="Senha" onChange={handleFormDataChange} required />
       </div>
-      <Link to="/admin">
-      <button className="login-button">Entrar</button>
-      </Link>
+      
+      <button type='submit' className="login-button" >Entrar</button>
+        </form>
+      
       
     </div>
     </div>
   );
 }
 
-export default Login;
+export default LoginAdm;

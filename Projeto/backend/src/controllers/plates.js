4b152aa15f8b51f83bc09488aaf42cf3492dcdca
Adriana Sousa <120699927+Adriana-Sousa@users.noreply.github.com>
2025-06-01 01:57:31 -0300

    update plates controller

--- Projeto/backend/src/controllers/plates.js
+++ Projeto/backend/src/controllers/plates.js
@@ -1,58 +1,68 @@
-import PlatesDataAccess from "../models/plates.js"
-import { ok, serverError } from "../helpers/httpResponses.js"
+import PlatesDataAccess from '../models/plates.js';
+import { ok, created, notFound, badRequest, serverError } from '../helpers/httpResponses.js';
 
 export default class PlatesControllers {
-    constructor() {
-        this.dataAccess = new PlatesDataAccess()
+  constructor() {
+    this.dataAccess = new PlatesDataAccess();
+  }
+
+  async getPlates() {
+    try {
+      const plates = await this.dataAccess.getPlates();
+      return ok(plates);
+    } catch (error) {
+      return serverError(error.message);
     }
+  }
 
-    async getPlates() {
-        try {
-            const plates = await this.dataAccess.getPlates()
-
-            return ok(plates)
-        } catch (error) {
-            return serverError(error)
-        }
+  async getAvailablePlates() {
+    try {
+      const plates = await this.dataAccess.getAvailablePlates();
+      return ok(plates);
+    } catch (error) {
+      return serverError(error.message);
     }
+  }
 
-    async getAvailablePlates() {
-        try {
-            const plates = await this.dataAccess.getAvailablePlates()
-
-            return ok(plates)
-        } catch (error) {
-            return serverError(error)
-        }
+  async addPlate(plateData) {
+    try {
+      const result = await this.dataAccess.addPlate(plateData);
+      return created(result);
+    } catch (error) {
+      if (error.message.includes('obrigatórios') || error.message.includes('inválida')) {
+        return badRequest(error.message);
+      }
+      return serverError(error.message);
     }
+  }
 
-    async addPlate(plateData) {
-        try {
-            const result = await this.dataAccess.addPlate(plateData)
-
-            return ok(result)
-        } catch (error) {
-            return serverError(error)
-        }
+  async deletePlate(plateId) {
+    try {
+      const result = await this.dataAccess.deletePlate(plateId);
+      return ok(result);
+    } catch (error) {
+      if (error.message.includes('não encontrado')) {
+        return notFound(error.message);
+      }
+      if (error.message.includes('ID inválido')) {
+        return badRequest(error.message);
+      }
+      return serverError(error.message);
     }
+  }
 
-    async deletePlate(plateId) {
-        try {
-            const result = await this.dataAccess.deletePlate(plateId)
-
-            return ok(result)
-        } catch (error) {
-            return serverError(error)
-        }
+  async updatePlate(plateId, plateData) {
+    try {
+      const result = await this.dataAccess.updatePlate(plateId, plateData);
+      return ok(result);
+    } catch (error) {
+      if (error.message.includes('não encontrado')) {
+        return notFound(error.message);
+      }
+      if (error.message.includes('obrigatórios') || error.message.includes('inválida')) {
+        return badRequest(error.message);
+      }
+      return serverError(error.message);
     }
-
-    async updatePlate(plateId, plateData) {
-        try {
-            const result = await this.dataAccess.updatePlate(plateId, plateData)
-
-            return ok(result)
-        } catch (error) {
-            return serverError(error)
-        }
-    }
+  }
 }

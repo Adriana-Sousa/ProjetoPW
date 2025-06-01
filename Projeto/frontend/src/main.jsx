1b70eec4bdeb24aef55e796b583da20194657f61
Adriana Sousa <120699927+Adriana-Sousa@users.noreply.github.com>
2025-06-01 02:22:30 -0300

    Update main

--- Projeto/frontend/src/main.jsx
+++ Projeto/frontend/src/main.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+
 import { CarrinhoProvider } from './context/carrinhoProvider.jsx';
+import { AuthProvider } from './context/authContext.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CarrinhoProvider>
-      <App />
-    </CarrinhoProvider>
+    <AuthProvider>
+      <CarrinhoProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </CarrinhoProvider>
+    </AuthProvider>
   </React.StrictMode>
-);
+)

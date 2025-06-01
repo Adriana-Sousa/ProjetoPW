1825c444943ee3e6d04dfd59870fc1196e4d2e2d
Adriana Sousa <120699927+Adriana-Sousa@users.noreply.github.com>
2025-06-01 02:19:15 -0300

    proteger as rotas

--- Projeto/frontend/src/App.jsx
+++ Projeto/frontend/src/App.jsx
@@ -1,7 +1,6 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
 import Layout from './components/layout/layout';
-//import Header from './components/header/header.jsx';
 import Footer from './components/footer/footer.jsx';
 import Home from './pages/home/home.jsx';
 import Carrinho from './pages/cart/cart.jsx';
@@ -11,25 +10,50 @@ import Cadastro from './pages/cadastro/cadastro.jsx';
 import Admin from './pages/admin/admin.jsx';
 import LoginAdm from './pages/loginadm/loginadm.jsx';
 import CardapioPublico from './pages/cardapio/cardapioPublico.jsx';
+import Notfound from './pages/notFound.jsx';
+import Unauthorized from './pages/Unauthorized.jsx';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
-  <Router>
-  <Layout>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/cart" element={<Carrinho />} />
-      <Route path="/cardapio" element={<Cardapio />} />
-      <Route path="/cardapio-publico" element={<CardapioPublico />} />
-      <Route path="/login" element={<Login/>}/>
-      <Route path="/cadastro" element={<Cadastro/>}/>
-      <Route path="/admin" element={<Admin/>}/>
-      <Route path="/loginadm" element={<LoginAdm/>}/>
-    </Routes>
-    <Footer />
-  </Layout>
-</Router>
-)
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/cart"
+          element={
+            <ProtectedRoute allowedRole="client">
+              <Carrinho />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/cardapio"
+          element={
+            <ProtectedRoute allowedRole="client">
+              <Cardapio />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/cardapio-publico" element={<CardapioPublico />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute allowedRole="admin">
+              <Admin />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/loginadm" element={<LoginAdm />} />
+        <Route path="/sem-autorizacao" element={<Unauthorized />} />
+        <Route path="/*" element={<Notfound />} />
+      </Routes>
+      <Footer />
+    </Layout>
+  );
 }
 
-export default App
+export default App;
+

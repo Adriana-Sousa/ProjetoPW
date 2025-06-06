b14fc33939d3a44040fa44cd8c82d33677bab162
Natália Lima <107011521+NattLima@users.noreply.github.com>
2025-06-05 23:18:17 -0300

    Update home.jsx

--- Projeto/frontend/src/pages/home/home.jsx
+++ Projeto/frontend/src/pages/home/home.jsx
@@ -1,36 +1,61 @@
 import { Link } from 'react-router-dom';
 import './home.css';
 import bgImage from '../../assets/HOME.jpg';
-import { autenticado } from '../../context/authContext';
-import { adminRole } from '../../context/authContext';
+import { useAuth } from '../../hooks/useAuth';
 import AdminPage from '../admin/admin';
-import Cardapio from '../cardapio/cardapio';
-
+import Cardapio from '../cardapio/cardapioUser';
+import { useEffect, useState } from 'react';
 
 function Home() {
+  const { isAuthenticated, user, success, setSuccess} = useAuth();
+  const [showPopup, setShowPopup] = useState(false);
+
+  useEffect(() => {
+    if (success) {
+      setShowPopup(true);
+      const timer = setTimeout(() => {
+        setShowPopup(false);
+        setSuccess(null); 
+      }, 2000);
+      return () => clearTimeout(timer);
+    }
+  }, [success, setSuccess]);
+
   return (
-  <>
-  {
-          autenticado?  adminRole? <AdminPage /> : <Cardapio /> : <div 
-      className="home-page" 
-      style={{ backgroundImage: `url(${bgImage})` }}
-    >
-      <div className="home-container">
-        <h1>It's Tasty</h1>
-        <Link to="/login">
-          <button className="home-button">Entrar</button>
-        </Link>
-        <Link to="/cardapio-publico">
-          <button className="home-button">Ver Cardápio</button>
-        </Link>
-        <p>
-          Não tem uma conta? <Link to="/cadastro">Cadastre-se</Link>
-        </p>
-      </div>
-    </div> 
-        }
-  </>
-  
+    <>
+      {isAuthenticated ? (
+        user?.role === 'admin' ? (
+          <AdminPage />
+        ) : (
+          <Cardapio />
+        )
+      ) : (
+        <div
+          className="home-page"
+          style={{ backgroundImage: `url(${bgImage})` }}
+        >
+          <div className="home-container">
+            <h1>It's Tasty</h1>
+            <Link to="/login">
+              <button className="home-button">Entrar</button>
+            </Link>
+            <Link to="/cardapio-publico">
+              <button className="home-button">Ver Cardápio</button>
+            </Link>
+            <p>
+              Não tem uma conta? <Link to="/cadastro">Cadastre-se</Link>
+            </p>
+            {showPopup && (
+              <div className="popup">
+                <div className="popup-content">
+                  <p>{success}</p>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 

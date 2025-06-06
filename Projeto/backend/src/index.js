74e939bd7a74da64f5d7409489a6f45c2e646219
Adriana Sousa <120699927+Adriana-Sousa@users.noreply.github.com>
2025-06-06 15:31:56 -0300

    update index

--- Projeto/backend/src/index.js
+++ Projeto/backend/src/index.js
@@ -1,40 +1,31 @@
 import express from 'express'
 import cors from 'cors'
 import { Mongo } from './database/mongo.js'
-import dotenv from 'dotenv'
+import { config } from 'dotenv' 
 import authRouter from './auth/auth.js'
 import usersRouter from './routes/users.js'
 import platesRouter from './routes/plates.js'
+import ordersRouter from './routes/orders.js'
 
-dotenv.config()
-
+config()
 async function main() {
     const hostname = 'localhost'
-    const port = process.env.PORT || 3000
+    const port = 3000
 
     const app = express()
 
-    const mongoConnection = await Mongo.connect({
-        mongoConnectionString: process.env.MONGO_CS,
-        mongoDbName: process.env.MONGO_DB_NAME
-    })
-    
-    if (mongoConnection?.error) {
-        console.error(mongoConnection)
-        process.exit(1)
-    }
-
+    // o env armazena a string de conecao e o dbname
+    const mongoConnection = await Mongo.connect(
+        { mongoConnectionString: process.env.MONGO_CS, mongoDbName: process.env.MONGO_DB_NAME })
     console.log(mongoConnection)
-
+    
     app.use(express.json())
-    app.use(cors({
-        origin: 'http://localhost:5173',
-        credentials: true
-    }))
+    app.use(cors())
 
+    // rota 
     app.get('/', (req, res) => {
         res.send({
-            success: true,
+            success: true, 
             statusCode: 200,
             body: 'Welcome to Appname!'
         })
@@ -43,6 +34,7 @@ async function main() {
     app.use('/auth', authRouter)
     app.use('/users', usersRouter)
     app.use('/plates', platesRouter)
+    app.use('/orders', ordersRouter)
 
     app.listen(port, () => {
         console.log(`Server running on: http://${hostname}:${port}`)
@@ -50,7 +42,3 @@ async function main() {
 }
 
 main()
-.catch(error => {
-    console.error('Error starting the server:', error)
-    process.exit(1)
-})

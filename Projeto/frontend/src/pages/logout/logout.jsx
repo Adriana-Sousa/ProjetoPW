ecb390cfea0030521b6f10692cd4932f29f7c7aa
Natália Lima <107011521+NattLima@users.noreply.github.com>
2025-06-05 23:25:43 -0300

    Add files via upload

---
+++ Projeto/frontend/src/pages/logout/logout.jsx
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom';
+import './logout.css';
+import bgImage from '../../assets/HOME.jpg';
+
+function Logout() {
+  return (
+    <div
+      className="logout-page"
+      style={{ backgroundImage: `url(${bgImage})` }}
+    >
+      <div className="logout-container">
+        <h2>Você realizou logout</h2>
+        <Link to="/login">
+          <button className="logout-button">Fazer login de novo</button>
+        </Link>
+        <Link to="/">
+          <button className="logout-button">Ir para página inicial</button>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Logout;

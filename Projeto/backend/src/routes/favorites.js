6a6976042e46215d06486509279575ed3355cbef
Adriana Sousa <120699927+Adriana-Sousa@users.noreply.github.com>
2025-06-11 20:52:31 -0300

    add route update favorites

--- Projeto/backend/src/routes/favorites.js
+++ Projeto/backend/src/routes/favorites.js
@@ -27,6 +27,19 @@ favoritesRouter.post('/:plateId', isAuthenticated, async (req, res) => {
   }
 });
 
+favoritesRouter.put('/', isAuthenticated, async (req, res) => {
+  try {
+    const { plateIds } = req.body;
+    const { body, success, statusCode } = await favoritesControllers.updateFavorites(
+      req.user.userId,
+      plateIds
+    );
+    res.status(statusCode).send({ body, success, statusCode });
+  } catch (error) {
+    res.status(500).send(serverError(error.message));
+  }
+});
+
 favoritesRouter.delete('/:plateId', isAuthenticated, async (req, res) => {
   try {
     const { body, success, statusCode } = await favoritesControllers.removeFavorite(
